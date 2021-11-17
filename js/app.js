8a981b74b1a0713e56662ad0107281cd524a777a
martingdinardi <martingdinardi@gmail.com>
2021-11-17 19:53:54 -0300

    .3 deck numbers done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-alert("funciono");
+let deck = [];
+
+for (let i = 2; i < 11; i++) {
+  for (let s = 0; s < 4; s++) {
+    let suit = ["C", "D", "P", "T"];
+    deck.push(`${i}${suit[s]}`);
+  }
+}
+
+console.log(deck);

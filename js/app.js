5b9478d24d386ddfe88c0d554215351ddc3193c9
martingdinardi <martingdinardi@gmail.com>
2021-11-18 21:41:03 -0300

    .4 deck done, and shuffle deck created

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 let deck = [];
 
-for (let i = 2; i < 11; i++) {
-  for (let s = 0; s < 4; s++) {
-    let suit = ["C", "D", "P", "T"];
-    deck.push(`${i}${suit[s]}`);
+let shuffle_deck = () => {
+  for (let i = 2; i < 11; i++) {
+    for (let s = 0; s < 4; s++) {
+      let suit = ["C", "D", "P", "T"];
+      deck.push(`${i}${suit[s]}`);
+    }
   }
-}
+
+  for (let i = 0; i < 4; i++) {
+    for (let s = 0; s < 4; s++) {
+      let suit = ["C", "D", "P", "T"];
+      let letter = ["J", "Q", "K", "A"];
+      deck.push(`${letter[i]}${suit[s]}`);
+    }
+  }
+};
+
+shuffle_deck();
 
 console.log(deck);
